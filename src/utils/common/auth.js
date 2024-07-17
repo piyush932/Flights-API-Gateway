b313b83a513f186ef0d815fc91c644dca9fca842
Piyush <b521044@iiit-bh.ac.in>
2024-07-18 01:30:00 +0530

    add disauthenticated middleware

--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -18,7 +18,17 @@ function createToken(input) {
     }
 }
 
+function verifyToken(token){
+    try {
+        return jwt.verify(token, ServerConfig.JWT_SECRET,{expiresIn: ServerConfig.JWT_EXPIRY});
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 module.exports = {
     checkPassword,
-    createToken
+    createToken,
+    verifyToken
 }
