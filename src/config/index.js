8058743d2cb67987a9d6bc2852544cdc13e7e511
Piyush <b521044@iiit-bh.ac.in>
2024-07-18 17:33:14 +0530

    add rate limiter and proxy

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
+const { JWT_SECRET } = require('./server-config');
+
 module.exports = {
     ServerConfig: require('./server-config'),
-    Logger: require('./logger-config')
+    Logger: require('./logger-config'),
 }
